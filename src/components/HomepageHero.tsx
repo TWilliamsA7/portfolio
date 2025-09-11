4394de823518d062069e9a9deaa81230e9c58bdb
BatsCrow <156543086+BatsCrow@users.noreply.github.com>
2025-09-10 22:57:14 -0400

    Initializing

---
+++ src/components/HomepageHero.tsx
@@ -0,0 +1,9 @@
+export default function HomepageHero() {
+    return (
+        <div className="flex flex-col min-w-screen min-h-screen bg-amber-300 justify-center">
+            <div className="text-center xl:text-5xl lg:text-3xl md:text-2xl text-xl sm:text-red-700 text-primary">
+                Tai Williams
+            </div>
+        </div>
+    )
+}
