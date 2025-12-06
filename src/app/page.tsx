e6136b14802e7411ed3268bd311364f5f1adcae1
BatsCrow <twilliamsa776@gmail.com>
2025-12-06 16:51:20 -0500

    ADD: Initial About Me Redo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-
-import AboutSection from "@/components/about-section";
+import AboutSection from "@/components/about-section-v2";
 import HomepageHero from "@/components/HomepageHero";
-import  MainNavbar from "@/components/main-navbar";
+import MainNavbar from "@/components/main-navbar";
 
 export default function Home() {
   return (
@@ -13,7 +12,9 @@ export default function Home() {
 
       {/* Placeholder content to enable scrolling and test the navbar interaction */}
       <div className="h-[200vh] flex items-center justify-center">
-        <h2 className="text-3xl text-muted-foreground">More Content Below to Test Scroll...</h2>
+        <h2 className="text-3xl text-muted-foreground">
+          More Content Below to Test Scroll...
+        </h2>
       </div>
     </main>
   );
