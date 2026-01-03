3f97776c82617e8f66f010a5a24fe92036e4f078
BatsCrow <twilliamsa776@gmail.com>
2026-01-03 12:16:09 -0500

    Alternating rows

--- src/components/skills/SkillRow.tsx
+++ src/components/skills/SkillRow.tsx
@@ -4,37 +4,59 @@
 
 import { Skill, skills, SkillType } from "@/data/skills";
 import { useAnimationFrame } from "motion/react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SkillIcon from "@/components/skills/SkillIcon";
+import { cn } from "@/lib/utils";
 
 interface SkillsRowProps {
+  index: number;
   category: SkillType;
   className?: string;
 }
 
-export function SkillsRow({ category, className }: SkillsRowProps) {
+export function SkillsRow({ category, className, index }: SkillsRowProps) {
   const containerRef = useRef<HTMLDivElement>(null);
+  const viewportRef = useRef<HTMLDivElement>(null);
   const [isHovered, setIsHovered] = useState<boolean>(false);
   const [direction, setDirection] = useState<number>(1);
+  const [shouldScroll, setShouldScroll] = useState(false);
 
   const x = useRef(0);
+
   const categorySkills: Skill[] = skills.filter(
     (s) => s.skill_type === category
   );
+  const displaySkills: Skill[] = shouldScroll
+    ? [...categorySkills, ...categorySkills]
+    : categorySkills;
+
+  const MIN_ICONS_FOR_SCROLL: number = 6;
+
+  useEffect(() => {
+    setShouldScroll(categorySkills.length > MIN_ICONS_FOR_SCROLL);
+  }, [categorySkills]);
+
+  useEffect(() => {
+    setDirection(index % 2 ? -1 : 1);
+  }, [index]);
 
   useAnimationFrame((_, delta) => {
-    if (isHovered) return;
+    if (isHovered || !shouldScroll) return;
 
     const speed = 0.03 * direction;
-    x.current -= speed * delta;
+    x.current += speed * delta;
 
     const container = containerRef.current;
     if (!container) return;
 
     const width = container.scrollWidth / 2;
 
-    if (Math.abs(x.current) >= width) {
+    if (direction < 0 && x.current <= -width) {
       x.current = 0;
+    }
+
+    if (direction > 0 && x.current >= 0) {
+      x.current = -width;
     }
 
     container.style.transform = `translateX(${x.current}px)`;
@@ -55,8 +77,14 @@ export function SkillsRow({ category, className }: SkillsRowProps) {
         <div className="pointer-events-none absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-background to-transparent z-10" />
 
         {/* Scrolling track */}
-        <div ref={containerRef} className="flex w-max gap-8 py-4">
-          {[...categorySkills, ...categorySkills].map((skill, idx) => (
+        <div
+          ref={containerRef}
+          className={cn(
+            "flex gap-8 py-4",
+            shouldScroll ? "w-max" : "w-full justify-center"
+          )}
+        >
+          {displaySkills.map((skill, idx) => (
             <SkillIcon
               key={`${skill.name}-${idx}`}
               skill={skill}
