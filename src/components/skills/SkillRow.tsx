027d54ed65410a1ee6d8f44762e15ca854c1ff02
BatsCrow <twilliamsa776@gmail.com>
2026-01-03 11:23:33 -0500

    Oscillating skills row

--- src/components/skills/SkillRow.tsx
+++ src/components/skills/SkillRow.tsx
@@ -2,21 +2,30 @@
 
 "use client";
 
-import { skills } from "@/data/skills";
-import { motion, useAnimationFrame } from "motion/react";
+import { Skill, skills, SkillType } from "@/data/skills";
+import { useAnimationFrame } from "motion/react";
 import { useRef, useState } from "react";
 import SkillIcon from "@/components/skills/SkillIcon";
 
-export function SkillsRow() {
+interface SkillsRowProps {
+  category: SkillType;
+  className?: string;
+}
+
+export function SkillsRow({ category, className }: SkillsRowProps) {
   const containerRef = useRef<HTMLDivElement>(null);
   const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [direction, setDirection] = useState<number>(1);
 
   const x = useRef(0);
+  const categorySkills: Skill[] = skills.filter(
+    (s) => s.skill_type === category
+  );
 
   useAnimationFrame((_, delta) => {
     if (isHovered) return;
 
-    const speed = 0.03;
+    const speed = 0.03 * direction;
     x.current -= speed * delta;
 
     const container = containerRef.current;
@@ -25,7 +34,7 @@ export function SkillsRow() {
     const width = container.scrollWidth / 2;
 
     if (Math.abs(x.current) >= width) {
-      x.current = 0;
+      setDirection(-direction);
     }
 
     container.style.transform = `translateX(${x.current}px)`;
@@ -33,12 +42,13 @@ export function SkillsRow() {
 
   return (
     <div
-      className="relative overflow-hidden"
+      className={`relative overflow-hidden flex-col items-center ${className}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div ref={containerRef} className="flex -w-max gap-6 py-4">
-        {skills.map((skill, idx) => (
+      <h2 className="text-xl text-primary font-bold py-4">{category}</h2>
+      <div ref={containerRef} className="flex w-1/2 gap-6 py-4">
+        {[...categorySkills, ...categorySkills].map((skill, idx) => (
           <SkillIcon
             key={`${skill.name}-${idx}`}
             skill={skill}
