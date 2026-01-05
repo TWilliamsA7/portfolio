aa70bdacb58520890b150931f969d28e024cf320
BatsCrow <twilliamsa776@gmail.com>
2026-01-05 14:15:32 -0500

    Scene wrap with grid

---
+++ src/components/three/Scene.tsx
@@ -0,0 +1,11 @@
+// src/components/three/Scene.tsx
+
+import DepthGrid from "@/components/three/DepthGrid";
+
+export default function Scene() {
+  return (
+    <>
+      <DepthGrid />
+    </>
+  );
+}
