2b44c21cfdef5946a7ce98d6ab9f7e43534ce487
BatsCrow <twilliamsa776@gmail.com>
2026-01-05 19:19:24 -0500

    Theme responsive network and grid

--- src/components/three/DepthGrid.tsx
+++ src/components/three/DepthGrid.tsx
@@ -1,18 +1,21 @@
 // src/components/three/DepthGrid.tsx
 
 import { Grid } from "@react-three/drei";
+import { useTheme } from "next-themes";
 
 export default function DepthGrid() {
+  const { theme } = useTheme();
+
   return (
     <Grid
       position={[0, -2, 0]}
       args={[20, 20]}
       cellSize={1}
       cellThickness={0.6}
-      cellColor="#ffffff"
+      cellColor={theme === "dark" ? "#ffffff" : "#000000"}
       sectionSize={5}
       sectionThickness={1}
-      sectionColor="#334155"
+      sectionColor={theme === "dark" ? "#ffffff" : "#000000"}
       fadeDistance={20}
       fadeStrength={1}
     />
