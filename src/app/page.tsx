0bc65b899155de2d1d9d0254f9bc2bcb4485286c
BatsCrow <twilliamsa776@gmail.com>
2026-01-06 12:52:53 -0500

    UI updates and resume fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 import HomepageHero from "@/components/homepage/HomepageHero";
-import ResumeButton from "@/components/homepage/resume-button";
 import AboutSection from "@/components/homepage/about-section";
-
 import { FeaturedProjectsSection } from "@/components/projects/featured-projects-section";
 import SkillsSection from "@/components/skills/SkillsSection";
-import ExperienceSection from "@/components/experience/ExperienceSection";
+import ExperienceButton from "@/components/homepage/experience-button";
 
 export default function Home() {
   return (
     <main>
       <HomepageHero />
       <AboutSection />
-
       <div className="flex items-center justify-center">
-        <ResumeButton />
+        <ExperienceButton />
       </div>
-
       <FeaturedProjectsSection />
-
       <SkillsSection />
     </main>
   );
--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -7,10 +7,9 @@ import { Separator } from "@/components/ui/separator";
 export default function SkillsSection() {
   return (
     <section>
-      <h1 className="text-2xl md:text-3xl font-bold text-primary text-center underline uppercase py-3">
+      <h1 className="text-2xl md:text-3xl font-bold text-primary text-center underline uppercase py-3 my-4">
         Skills
       </h1>
-      <Separator className="my-8" />
       {SkillTypes.map((st, idx) => (
         <SkillsRow category={st} key={idx} index={idx} />
       ))}
