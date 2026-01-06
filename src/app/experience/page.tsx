4d0429c1280459eb59076859d3adb3f00c7b4a41
BatsCrow <twilliamsa776@gmail.com>
2026-01-06 08:55:13 -0500

    experience page formatting

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,7 +1,15 @@
 // src/app/experience/page.tsx
 
 import ExperienceSection from "@/components/experience/ExperienceSection";
+import ResumeButton from "@/components/homepage/resume-button";
 
 export default function ExperiencePage() {
-  return <ExperienceSection />;
+  return (
+    <>
+      <ExperienceSection />
+      <div className="flex items-center justify-center">
+        <ResumeButton />
+      </div>
+    </>
+  );
 }
